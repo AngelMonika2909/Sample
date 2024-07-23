f26522916ab844470466c6925b2534ad058518cb
AngelMonika2909 <angelmonika.2909@gmail.com>
2024-07-23 19:29:11 +0530

    done

--- src/org/demo/EmployeeDetails.java
+++ src/org/demo/EmployeeDetails.java
@@ -5,4 +5,9 @@ public class EmployeeDetails {
 	private void emp1() {
 System.out.println("employee name : Monika ");
 	}
+	
+	private void emp3() {
+System.out.println("Employee Name : gershom");
+	}
+	
 }
